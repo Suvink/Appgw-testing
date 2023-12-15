141c285925a8cce695aee462b64568ade1a50da0
Suvin Nimnaka <suvin@wso2.com>
2023-12-15 09:43:16 +0530

    Add stytch auth impl

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StytchLogin } from "@stytch/react";
+import { StytchLogin, useStytchUser, useStytch } from "@stytch/react";
 import { Products } from "@stytch/vanilla-js";
 
 function Home() {
@@ -12,9 +12,51 @@ function Home() {
       signupExpirationMinutes: 60,
     }
   };
+  const stytch = useStytch();
+  const { user } = useStytchUser();
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    // Validate code
+    if (stytch && !user) {
+      const queryParams = new URLSearchParams(window.location.search);
+      const token = queryParams.get("token");
+      const tokenType = queryParams.get("stytch_token_type");
+
+      // If a token is found, authenticate it with the appropriate method
+      if (token && tokenType) {
+        if (tokenType === "magic_links") {
+          stytch.magicLinks.authenticate(token, {
+            session_duration_minutes: 60,
+          });
+        } else if (tokenType === "oauth") {
+          stytch.oauth.authenticate(token, {
+            session_duration_minutes: 60,
+          });
+        }
+      }
+    }
+  }, [stytch, user]);
+
+  const getAccessToken = async () => {
+    const sessionCookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("stytch_session_jwt"));
+    setAccessToken(sessionCookie.split("=")[1]);
+  }
+
   return (
     <div>
-      <StytchLogin config={config} />
+      {
+        user ? <div>
+          <h1>Welcome {user.name.first_name}</h1>
+          <button onClick={getAccessToken}>Get access token</button>
+          <div>
+            <pre>{accessToken}</pre>
+          </div>
+        </div> :
+          <StytchLogin config={config} />
+      }
     </div>
   );
 }
