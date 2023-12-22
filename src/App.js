87ea21aac8e562ef91de89be2ba3fd60d09b34d6
Suvin Nimnaka <suvin@wso2.com>
2023-12-22 10:08:22 +0530

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom"
+import { RequireSignIn } from "@rownd/react";
 import Home from './Home';
+import SecretPage from './SecretPage';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/secret" element={
+        <RequireSignIn>
+          <SecretPage />
+        </RequireSignIn>
+      } />
     </Routes>
   );
 }
